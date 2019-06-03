37692c16d687d5bdcc3b555b8a91967f5a167402
Markus Dittmann <dmarku@posteo.de>
2019-06-03 14:37:14 +0200

    handle mouse button presses

--- blog.md
+++ blog.md
@@ -5,5 +5,15 @@
 - goal: rebuild snake game in Rust and Piston
 - initialized hello world example with `cargo init`, test build with `cargo run`
 - first error: piston docs mention old version for dependencies
+
   - found current version with `cargo search piston_window` -> 0.94.0
   - updated build downloads & compiles _a lot_ of crates
+
+- follow (a piston "getting started" tutorial")[https://github.com/PistonDevelopers/Piston-Tutorials/tree/master/getting-started]
+
+  - the example code is _very_ different to piston's repository readme example
+  - learned about piston's modular architecture
+  - learned that glutin is a portable OpenGL context creation library in pure Rust, similar to glut
+  - learned that piston's graphics interface can be implemented with different backends
+
+- handle mouse button presses
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,37 @@ extern crate piston_window;
 use piston_window::*;
 
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("Sneake", [800, 600])
-    .exit_on_esc(true).build().unwrap();
+
+    let mut x = 50.0;
+    let mut y = 50.0;
+
+    let mut window: PistonWindow = WindowSettings::new("Sneake", (800, 600))
+        .exit_on_esc(true)
+        .graphics_api(OpenGL::V3_2)
+        .build()
+        .unwrap();
 
     let rect_color: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
     while let Some(event) = window.next() {
-        window.draw_2d(&event, |context, graphics, _device| {
-            clear([1.0; 4], graphics);
-            rectangle(rect_color, rectangle::square(0.0, 0.0, 50.0), context.transform, graphics);
-        });
+        if let Some(_) = event.render_args() {
+            window.draw_2d(&event, |context, graphics, _device| {
+                clear([1.0; 4], graphics);
+                rectangle(
+                    rect_color,
+                    rectangle::square(x, y, 50.0),
+                    context.transform,
+                    graphics,
+                );
+            });
+        }
+        if let Some(button) = event.press_args() {
+            if button == Button::Mouse(MouseButton::Left) {
+                x = x + 10.0;
+            }
+            if button == Button::Mouse(MouseButton::Right) {
+                x = x - 10.0;
+            }
+        }
     }
 }
