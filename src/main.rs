580d8450e37b2d0e25a5f135a8468c12c9e35da3
Markus Dittmann <dmarku@posteo.de>
2019-06-03 13:12:08 +0200

    draw a red rectangle!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate piston_window;
+
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window: PistonWindow = WindowSettings::new("Sneake", [800, 600])
+    .exit_on_esc(true).build().unwrap();
+
+    let rect_color: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |context, graphics, _device| {
+            clear([1.0; 4], graphics);
+            rectangle(rect_color, rectangle::square(0.0, 0.0, 50.0), context.transform, graphics);
+        });
+    }
 }
